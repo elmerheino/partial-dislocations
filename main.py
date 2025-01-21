cc141dc9c01be6630594bbd574ee5662f54eb891
elmerheino <elmer.heino2@gmail.com>
2025-01-21 14:44:43 +0200

    better

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ h0 = np.random.rand(10)
 def timestep(dt, h):
     random_noise = np.random.normal(0,0.1,10)
     tension = 1
-    res = np.gradient(np.gradient(h)) + random_noise + tension
+    res = (np.gradient(np.gradient(h)) + random_noise + tension)*dt
     return res
 
 h = [h0]
-for i in range(1,100):
+for i in range(1,1000):
     h_previous = h[i-1]
     h_i = timestep(0.2,h_previous)
     h.append(h_i)
@@ -19,7 +19,7 @@ plt.plot(h0)
 plt.plot(h[len(h)-1])
 
 plt.legend(
-    ["h10", "d^2h/dx^2"]
+    ["h0", "h1000"]
     )
 
 plt.show()
