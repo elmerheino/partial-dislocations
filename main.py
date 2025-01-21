3f3427c25b120d4cbd4447073effe29dbf58091e
elmerheino <elmer.heino2@gmail.com>
2025-01-21 22:33:23 +0200

    Added more reasonable interaction terms. Also plot of average distance.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-bigN = 100 # Number of discrete heights in the line
-length = 10
+bigN = 1024 # Number of discrete heights in the line
+length = 1024
 deltaL = length/bigN
 xVec = [i*deltaL for i in range(0,bigN)] # Come up with some x values, since the x corodinate is just the indice here.
 
+time = 250 # Time in seconds
 dt = 0.01
-time = 4000 # In number of timesteps of dt, so in this case, simulating 50s
-
+timesteps = round(time/dt) # In number of timesteps of dt
 
 bigB = 0.01
 smallB = 1
@@ -17,15 +17,26 @@ b_p = 0.01
 clt1 = 0.01
 clt2 = 0.01
 tauExt = 0.01
+c_gamma = 1
+d = 200 # Average distance
 
 def tau(x,y):
     return np.random.rand(bigN) # len(x) = len(y) = bigN
 
 def force1(y1,y2):
-    return -(y1-y2) # Now only adjacent nodes have an effect on each other
+    #return -np.average(y1-y2)*np.ones(bigN)
+    #return -(c_gamma*mu*b_p**2/d)*(1-y1/d) # Vaid et Al B.10
+
+    factor = (1/d)*c_gamma*mu*b_p**2
+    return factor*(1 + (y1-y2)/d)
 
 def force2(y1,y2):
-    return (y1-y2)
+    #return np.average(y1-y2)*np.ones(bigN)
+    #return (c_gamma*mu*b_p**2/d)*(1-y1/d) # Vaid et Al B.10
+
+    factor = (1/d)*c_gamma*mu*b_p**2
+    return factor*(1 + (y2-y1)/d) # Term from Vaid et Al B.7
+
 
 def secondDerivateive(x):
     return np.gradient(np.gradient(x))
@@ -40,10 +51,12 @@ def timestep(dt, y1,y2):
 y10 = np.random.rand(bigN)+np.ones(bigN)*2 # Make sure its bigger than y2 to being with
 y1 = [y10]
 
-y20 = np.random.rand(bigN)
+y20 = np.random.rand(bigN)*0.2
 y2 = [y20]
 
-for i in range(1,time):
+averageDist = []
+
+for i in range(1,timesteps):
     y1_previous = y1[i-1]
     y2_previous = y2[i-1]
 
@@ -51,14 +64,21 @@ for i in range(1,time):
     y1.append(y1_i)
     y2.append(y2_i)
 
-plt.plot(y10)
-plt.plot(y1[len(y1)-1])
+    averageDist.append(np.average(y1_i-y2_i))
 
-plt.plot(y20)
-plt.plot(y2[len(y2)-1])
+# plt.plot(y10)
+# plt.plot(y1[len(y1)-1])
 
-plt.legend(
-    ["y1_0", "y1_1000", "y2_0", "y2_1000"]
-    )
+# plt.plot(y20)
+# plt.plot(y2[len(y2)-1])
 
+# plt.legend(
+#     ["y1_0", "y1_1000", "y2_0", "y2_1000"]
+#     )
+
+# plt.show()
+
+plt.plot([i*dt for i in range(1,timesteps)], averageDist)
+plt.xlabel("t (s)")
+plt.ylabel("Average distance")
 plt.show()
