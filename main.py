5bf621bcafbc909793463da65b9d90048c7310d6
elmerheino <elmer.heino2@gmail.com>
2025-01-26 19:27:56 +0200

    make code more clear

--- main.py
+++ main.py
@@ -4,30 +4,31 @@ import matplotlib.pyplot as plt
 bigN = 1024 # Number of discrete heights in the line
 length = 1024
 deltaL = length/bigN
-xVec = [i*deltaL for i in range(0,bigN)] # Come up with some x values, since the x corodinate is just the indice here.
 
-time = 60*10 # Time in seconds
-dt = 0.05
+time = 2500 # Time in seconds
+dt = 0.1
 timesteps = round(time/dt) # In number of timesteps of dt
 
 bigB = 1
 smallB = 1
 mu = 2
 b_p = 2
-cLT1 = 3
-cLT2 = 1
-tauExt = 1
-c_gamma = 1
+cLT1 = 2
+cLT2 = 2
+tauExt = 2
+c_gamma = 5
 d = 200 # Average distance
+
+# gamma = 60.5
+# d0 = (c_gamma*mu/gamma)*b_p**2
 
 stressField = np.zeros([bigN, bigN])
 
 def generateRandomTau():
     deltaR = 1
-    res = np.random.normal(0,deltaR,[bigN, bigN])
-    stressField = res
+    stressField = np.random.normal(0,deltaR,[bigN, bigN])
 
-def tau(x,y): # Should be static in time.
+def tau(y): # Should be static in time. The index is again the x coordinate here
     yDisc = (np.round(y).astype(int) & bigN ) - 1 # Round the y coordinate to an integer and wrap around bigN
     return stressField[np.arange(bigN), yDisc] # x is discrete anyways here
 
@@ -50,10 +51,13 @@ def secondDerivateive(x):
     return np.gradient(np.gradient(x))
 
 def timestep(dt, y1,y2):
-    dy1 = (cLT1*mu*(b_p**2)*secondDerivateive(y1) + b_p*tau(xVec,y1) + force1(y1, y2))*(bigB/smallB)*dt + (smallB/2)*tauExt*np.ones(bigN)
-    dy2 = (cLT2*mu*(b_p**2)*secondDerivateive(y2) + b_p*tau(xVec,y2) + force2(y1, y2))*(bigB/smallB)*dt + (smallB/2)*tauExt*np.ones(bigN)
+    dy1 = ( cLT1*mu*(b_p**2)*secondDerivateive(y1) + b_p*tau(y1) + force1(y1, y2) + (smallB/2)*tauExt*np.ones(bigN) )*( bigB/smallB )*dt 
+    dy2 = ( cLT2*mu*(b_p**2)*secondDerivateive(y2) + b_p*tau(y2) + force2(y1, y2) + (smallB/2)*tauExt*np.ones(bigN) )*( bigB/smallB )*dt
     
-    return (y1+dy1, y2+dy2)
+    newY1 = (y1 + dy1)
+    newY2 = (y2 + dy2)
+
+    return (newY1, newY2)
 
 
 def run_simulation():
@@ -78,16 +82,18 @@ def run_simulation():
         averageDist.append(np.average(y1_i-y2_i))
     return averageDist
 
-# run 6 simulations right away
+# run 4 simulations right away
 
-fig, axes = plt.subplots(2, 1, figsize=(12, 8))
+fig, axes = plt.subplots(2, 2, figsize=(12, 8))
 axes_flat = axes.ravel()
 
 x = [i*dt for i in range(1,timesteps)]
 
-for i in range(0,2):
+for i in range(0,4):
     avgI = run_simulation()
     axes_flat[i].plot(x,avgI)
+    axes_flat[i].set_xlabel("Time (s)")
+    axes_flat[i].set_ylabel("Average distance")
 
 plt.tight_layout()
 plt.show()
