7e770c31c84c416317c9ba02bb44b1be16f64b2e
elmerheino <elmer.heino2@gmail.com>
2025-01-27 13:55:21 +0200

    change parameters, improve readabiloty

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 bigN = 1024 # Number of discrete heights in the line so len(y1) = len(y2) = bigN
-length = 1024
-deltaL = length/bigN
+length = 1024 # Length L of the actual line
+deltaL = length/bigN # The dx value in x direction
 
-time = 2500 # Time in seconds
-dt = 0.1
+time = 5000 # Time in seconds
+dt = 0.05
 timesteps = round(time/dt) # In number of timesteps of dt
+
+deltaR = 1 # Parameters for the random noise
 
 bigB = 1
 smallB = 1
-mu = 2
-b_p = 2
-cLT1 = 2
+
+b_p = 1
+cLT1 = 2 # Parameters of the gradient term
 cLT2 = 2
+mu = 1
+
 tauExt = 2
-c_gamma = 5
-d = 200 # Average distance
+c_gamma = deltaR/1000 # Parameter in the interaction force, should be small
+d = 500 # Average distance
 
 # gamma = 60.5
 # d0 = (c_gamma*mu/gamma)*b_p**2
@@ -25,7 +29,6 @@ d = 200 # Average distance
 stressField = np.zeros([bigN, bigN])
 
 def generateRandomTau():
-    deltaR = 1
     stressField = np.random.normal(0,deltaR,[bigN, bigN])
 
 def tau(y): # Should be static in time. The index is again the x coordinate here
@@ -36,26 +39,31 @@ def force1(y1,y2):
     #return -np.average(y1-y2)*np.ones(bigN)
     #return -(c_gamma*mu*b_p**2/d)*(1-y1/d) # Vaid et Al B.10
 
-    factor = (1/d)*c_gamma*mu*b_p**2
+    factor = (1/d)*c_gamma*mu*(b_p**2)
     return factor*(1 + (y1-y2)/d)
 
 def force2(y1,y2):
     #return np.average(y1-y2)*np.ones(bigN)
     #return (c_gamma*mu*b_p**2/d)*(1-y1/d) # Vaid et Al B.10
 
-    factor = (1/d)*c_gamma*mu*b_p**2
+    factor = (1/d)*c_gamma*mu*(b_p**2)
     return factor*(1 + (y2-y1)/d) # Term from Vaid et Al B.7
 
 
 def secondDerivateive(x):
-    return np.gradient(np.gradient(x))
+    return np.gradient(np.gradient(x, deltaL), deltaL)
 
 def timestep(dt, y1,y2):
-    dy1 = ( cLT1*mu*(b_p**2)*secondDerivateive(y1) + b_p*tau(y1) + force1(y1, y2) + (smallB/2)*tauExt*np.ones(bigN) )*( bigB/smallB )*dt 
-    dy2 = ( cLT2*mu*(b_p**2)*secondDerivateive(y2) + b_p*tau(y2) + force2(y1, y2) + (smallB/2)*tauExt*np.ones(bigN) )*( bigB/smallB )*dt
+    dy1 = ( 
+        cLT1*mu*(b_p**2)*secondDerivateive(y1) # The gradient term
+        + b_p*tau(y1) # The random stress term
+        + force1(y1, y2) # Interaction force
+        + (smallB/2)*tauExt*np.ones(bigN) # The external stress term
+        ) * ( bigB/smallB )
+    dy2 = ( cLT2*mu*(b_p**2)*secondDerivateive(y2) + b_p*tau(y2) + force2(y1, y2) + (smallB/2)*tauExt*np.ones(bigN) )*( bigB/smallB )
     
-    newY1 = (y1 + dy1)
-    newY2 = (y2 + dy2)
+    newY1 = (y1 + dy1*dt)
+    newY2 = (y2 + dy2*dt)
 
     return (newY1, newY2)
 
@@ -87,9 +95,10 @@ def run_simulation():
 fig, axes = plt.subplots(2, 2, figsize=(12, 8))
 axes_flat = axes.ravel()
 
-x = [i*dt for i in range(1,timesteps)]
+x = np.arange(timesteps-1)*dt
 
 for i in range(0,4):
+    np.random.seed(i)
     avgI = run_simulation()
     axes_flat[i].plot(x,avgI)
     axes_flat[i].set_xlabel("Time (s)")
