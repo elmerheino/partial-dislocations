0ea5e5c3e8248be6da86af64a5b7acd093d9b7a5
elmerheino <elmer.heino2@gmail.com>
2025-01-29 12:52:03 +0200

    Adjust parameters and potentials

--- main.py
+++ main.py
@@ -5,33 +5,35 @@ bigN = 1024 # Number of discrete heights in the line so len(y1) = len(y2) = bigN
 length = 1024 # Length L of the actual line
 deltaL = length/bigN # The dx value in x direction
 
-time = 5000 # Time in seconds
-dt = 0.05
+time = 1000 # Time in seconds
+dt = 0.5
 timesteps = round(time/dt) # In number of timesteps of dt
 
 deltaR = 1 # Parameters for the random noise
 
-bigB = 1
-smallB = 1
+bigB = 1    # Bulk modulus
+smallB = 1  # Size of Burgers vector
 
 b_p = 1
 cLT1 = 2 # Parameters of the gradient term
 cLT2 = 2
 mu = 1
 
-tauExt = 2
-c_gamma = deltaR/1000 # Parameter in the interaction force, should be small
-d = 500 # Average distance
+tauExt = 0
+
+c_gamma = deltaR/10 # Parameter in the interaction force, should be small
+
+d = 200 # Average distance
 
 # gamma = 60.5
 # d0 = (c_gamma*mu/gamma)*b_p**2
+# d = d0
 
-stressField = np.zeros([bigN, bigN])
 
 def generateRandomTau():
-    stressField = np.random.normal(0,deltaR,[bigN, bigN])
+    return np.random.normal(0,deltaR,[bigN, bigN])
 
-def tau(y): # Should be static in time. The index is again the x coordinate here
+def tau(y, stressField): # Should be static in time. The index is again the x coordinate here
     yDisc = (np.round(y).astype(int) & bigN ) - 1 # Round the y coordinate to an integer and wrap around bigN
     return stressField[np.arange(bigN), yDisc] # x is discrete anyways here
 
@@ -40,27 +42,27 @@ def force1(y1,y2):
     #return -(c_gamma*mu*b_p**2/d)*(1-y1/d) # Vaid et Al B.10
 
     factor = (1/d)*c_gamma*mu*(b_p**2)
-    return factor*(1 + (y1-y2)/d)
+    return factor*(1 + (y2-y1)/d)
 
 def force2(y1,y2):
     #return np.average(y1-y2)*np.ones(bigN)
     #return (c_gamma*mu*b_p**2/d)*(1-y1/d) # Vaid et Al B.10
 
-    factor = (1/d)*c_gamma*mu*(b_p**2)
+    factor = -(1/d)*c_gamma*mu*(b_p**2)
     return factor*(1 + (y2-y1)/d) # Term from Vaid et Al B.7
 
 
 def secondDerivateive(x):
     return np.gradient(np.gradient(x, deltaL), deltaL)
 
-def timestep(dt, y1,y2):
+def timestep(dt, y1,y2, stressField):
     dy1 = ( 
         cLT1*mu*(b_p**2)*secondDerivateive(y1) # The gradient term
-        + b_p*tau(y1) # The random stress term
+        + b_p*tau(y1, stressField) # The random stress term
         + force1(y1, y2) # Interaction force
         + (smallB/2)*tauExt*np.ones(bigN) # The external stress term
         ) * ( bigB/smallB )
-    dy2 = ( cLT2*mu*(b_p**2)*secondDerivateive(y2) + b_p*tau(y2) + force2(y1, y2) + (smallB/2)*tauExt*np.ones(bigN) )*( bigB/smallB )
+    dy2 = ( cLT2*mu*(b_p**2)*secondDerivateive(y2) + b_p*tau(y2, stressField) + force2(y1, y2) + (smallB/2)*tauExt*np.ones(bigN) )*( bigB/smallB )
     
     newY1 = (y1 + dy1*dt)
     newY2 = (y2 + dy2*dt)
@@ -69,40 +71,65 @@ def timestep(dt, y1,y2):
 
 
 def run_simulation():
-    y10 = np.random.rand(bigN)+np.ones(bigN)*d # Make sure its bigger than y2 to being with, and also that they have the initial distance d
+    y10 = np.ones(bigN)*d # Make sure its bigger than y2 to being with, and also that they have the initial distance d
     y1 = [y10]
 
-    y20 = np.random.rand(bigN)
+    y20 = np.zeros(bigN)
     y2 = [y20]
 
     averageDist = []
 
-    generateRandomTau()
+    stressField = generateRandomTau()
 
     for i in range(1,timesteps):
         y1_previous = y1[i-1]
         y2_previous = y2[i-1]
 
-        (y1_i, y2_i) = timestep(dt,y1_previous,y2_previous)
+        (y1_i, y2_i) = timestep(dt,y1_previous,y2_previous, stressField)
         y1.append(y1_i)
         y2.append(y2_i)
 
         averageDist.append(np.average(y1_i-y2_i))
     return averageDist
 
+def jotain_saatoa_potentiaaleilla():
+    forces_f1 = list()
+    forces_f2 = list()
+
+    for i in range(0,300):
+        y10 = np.ones(1)*i # Generate lines at distance i apart with length 1
+        y20 = np.zeros(1)
+
+        f1 = force1(y10,y20)
+        f2 = force2(y10,y20)
+
+        forces_f1.append(f1)
+        forces_f2.append(f2)
+    
+    plt.plot(forces_f1)
+    plt.plot(forces_f2)
+
+    plt.legend(["f_1", "f_2"])
+
+    plt.show()
+    pass
+
 # run 4 simulations right away
+def run4sims():
+    fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+    axes_flat = axes.ravel()
 
-fig, axes = plt.subplots(2, 2, figsize=(12, 8))
-axes_flat = axes.ravel()
+    x = np.arange(timesteps-1)*dt
 
-x = np.arange(timesteps-1)*dt
+    for i in range(0,4):
+        np.random.seed(i)
+        avgI = run_simulation()
+        axes_flat[i].plot(x,avgI)
+        axes_flat[i].set_xlabel("Time (s)")
+        axes_flat[i].set_ylabel("Average distance")
 
-for i in range(0,4):
-    np.random.seed(i)
-    avgI = run_simulation()
-    axes_flat[i].plot(x,avgI)
-    axes_flat[i].set_xlabel("Time (s)")
-    axes_flat[i].set_ylabel("Average distance")
+    plt.tight_layout()
+    plt.show()
 
-plt.tight_layout()
-plt.show()
+if __name__ == "__main__":
+    run4sims()
