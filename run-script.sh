75ea675cfececea3d1c6fad0befab6a3532af6a4
elmerheino <elmer.heino2@gmail.com>
2025-03-27 16:08:11 +0200

    fix error

--- run-script.sh
+++ run-script.sh
@@ -19,12 +19,12 @@ NOISE_MAX=2
 # Seed count is array-max/noise points
 
 # Job step
-srun python3 main.py --array-task-id ${SLURM_ARRAY_TASK_ID} -rmin ${NOISE_MIN} -rmax ${NOISE_MAX} \
+srun python3 main.py --array-task-id ${SLURM_ARRAY_TASK_ID} --rmin ${NOISE_MIN} --rmax ${NOISE_MAX} \
     --array-length 100 --seeds 10 -p 100 -dt 0.05 \
     -f ${WRKDIR}/results/${DATE}/single-dislocation -c ${CORES} \
     -t 10000 --single \
 
-srun python3 main.py --array-task-id ${SLURM_ARRAY_TASK_ID} -rmin ${NOISE_MIN} -rmax ${NOISE_MAX} \
+srun python3 main.py --array-task-id ${SLURM_ARRAY_TASK_ID} --rmin ${NOISE_MIN} --rmax ${NOISE_MAX} \
     --array-length 100 --seeds 10 -p 100 -dt 0.05 \
     -f ${WRKDIR}/results/${DATE}/partial-dislocation -c ${CORES} \
     -t 10000 --partial \
