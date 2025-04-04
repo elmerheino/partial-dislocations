991d1ddeb439ef8ea8b2eb7667a14caf81adfcc2
elmerheino <elmer.heino2@gmail.com>
2025-04-04 14:13:36 +0200

    Update run-script.sh

--- run-script.sh
+++ run-script.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=01:30:00
 #SBATCH --mem-per-cpu=10G
 #SBATCH --output=depinning.out
-#SBATCH --array=0-10000
+#SBATCH --array=0-100
 #SBATCH --cpus-per-task=9
 
 DATE=$(date +"%Y-%m-%d")
@@ -17,5 +17,5 @@ module load scicomp-python-env
 # Seed count is array-max/noise points
 
 # Job step
-srun python3 main.py --seed ${SEED} -R ${NOISE} -f ${WRKDIR}/results/${DATE}/partial-dislocation -tmin 2.65 -tmax 3.0 -p 100 -dt 0.05 -c ${CORES} -t 10000 --partial
-srun python3 main.py --seed ${SEED} -R ${NOISE}  -f ${WRKDIR}/results/${DATE}/single-dislocation -tmin 2.65 -tmax 3.0 -p 100 -dt 0.05 -c ${CORES} -t 10000 --single
+srun python3 main.py --seed ${SLURM_ARRAY_TASK_ID} -R ${NOISE} -f ${WRKDIR}/results/${DATE}-perfect-partial/partial-dislocation -tmin 2.65 -tmax 3.0 -p 100 -dt 0.05 -c ${CORES} -t 10000 --partial
+srun python3 main.py --seed ${SLURM_ARRAY_TASK_ID} -R ${NOISE} -f ${WRKDIR}/results/${DATE}-perfect-partial/single-dislocation -tmin 2.65 -tmax 3.0 -p 100 -dt 0.05 -c ${CORES} -t 10000 --single
