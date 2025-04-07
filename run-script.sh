f534a5365423b4c5312e1d694c75abe8cd35305d
elmerheino <elmer.heino2@gmail.com>
2025-04-07 10:58:40 +0200

    Update run-script.sh

--- run-script.sh
+++ run-script.sh
@@ -17,5 +17,5 @@ module load scicomp-python-env
 # Seed count is array-max/noise points
 
 # Job step
-srun python3 main.py --seed ${SLURM_ARRAY_TASK_ID} -R ${NOISE} -f ${WRKDIR}/results/${DATE}-perfect-partial/partial-dislocation -tmin 2.65 -tmax 3.0 -p 100 -dt 0.05 -c ${CORES} -t 10000 --partial
-srun python3 main.py --seed ${SLURM_ARRAY_TASK_ID} -R ${NOISE} -f ${WRKDIR}/results/${DATE}-perfect-partial/single-dislocation -tmin 2.65 -tmax 3.0 -p 100 -dt 0.05 -c ${CORES} -t 10000 --single
+srun python3 main.py --seed ${SLURM_ARRAY_TASK_ID} -R ${NOISE} -f ${WRKDIR}/results/${DATE}-perfect-partial/partial-dislocation -tmin 1 -tmax 2 -p 100 -dt 0.05 -c ${CORES} -t 10000 --partial
+srun python3 main.py --seed ${SLURM_ARRAY_TASK_ID} -R ${NOISE} -f ${WRKDIR}/results/${DATE}-perfect-partial/single-dislocation -tmin 1 -tmax 2 -p 100 -dt 0.05 -c ${CORES} -t 10000 --single
