5c974dbfb74694f7d3fe09f5e052cca67ea4679e
elmerheino <elmer.heino2@gmail.com>
2025-04-28 09:28:58 +0300

    Adjust run script

--- run-noise.sh
+++ run-noise.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --time=02:30:00
-#SBATCH --mem-per-cpu=10G
-#SBATCH --output=depinning.out
+#SBATCH --time=06:30:00
+#SBATCH --mem-per-cpu=9G
+#SBATCH --output=depinning-noise.out
 #SBATCH --array=1-100
-#SBATCH --cpus-per-task=9
+#SBATCH --cpus-per-task=20
 
 DATE=$(date +"%Y-%m-%d")
 echo $DATE
 
-CORES=9
+CORES=20
 
 # You may put the commands below:
 module load scicomp-python-env
@@ -16,10 +16,8 @@ module load scicomp-python-env
 NOISE_MIN=-2
 NOISE_MAX=2
 
-ARRAY_LEN=100 # SEEDS*SEEDS=ARRAY_LEN for a square grid
-SEEDS=1
-
-# Seed count is array-max/noise points
+ARRAY_LEN=100   # SEEDS*NOISES=ARRAY_LEN for a square grid
+SEEDS=1         # Seed count is array-max/noise points
 
 # Job step
 srun python3 main.py -p 100 -dt 0.05 -t 10000 --single -f ${WRKDIR}/results/${DATE}/single-dislocation -c ${CORES} \
