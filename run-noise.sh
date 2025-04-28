90e7138fceee077dbba78435f03575efedfd5241
elmerheino <elmer.heino2@gmail.com>
2025-04-28 13:07:56 +0300

    Adjust noise script

--- run-noise.sh
+++ run-noise.sh
@@ -19,9 +19,10 @@ NOISE_MAX=2
 ARRAY_LEN=100   # SEEDS*NOISES=ARRAY_LEN for a square grid
 SEEDS=1         # Seed count is array-max/noise points
 
-# Job step
-srun python3 main.py -p 100 -dt 0.05 -t 10000 --single -f ${WRKDIR}/results/${DATE}/single-dislocation -c ${CORES} \
+# Job step perfect dislocation
+srun python3 main.py -p 100 -dt 0.05 -t 10000 --single -f ${WRKDIR}/results/${DATE}-noise/single-dislocation -c ${CORES} \
     grid --array-task-id ${SLURM_ARRAY_TASK_ID} --rmin ${NOISE_MIN} --rmax ${NOISE_MAX} --array-length ${ARRAY_LEN} --seeds ${SEEDS}
 
-srun python3 main.py -p 100 -dt 0.05 -t 10000 --partial -f ${WRKDIR}/results/${DATE}/partial-dislocation -c ${CORES} \
+# Job step partial dislocation
+srun python3 main.py -p 100 -dt 0.05 -t 10000 --partial -f ${WRKDIR}/results/${DATE}-noise/partial-dislocation -c ${CORES} \
     grid --array-task-id ${SLURM_ARRAY_TASK_ID} --rmin ${NOISE_MIN} --rmax ${NOISE_MAX} --array-length ${ARRAY_LEN} --seeds ${SEEDS}
