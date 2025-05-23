663c0f984c1973bb359ce11a0cd373f821580ce2
elmerheino <elmer.heino2@gmail.com>
2025-05-23 09:53:02 +0300

    Update scripts

--- run-noise-partial.sh
+++ run-noise-partial.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --time=06:50:00
-#SBATCH --job-name=noise-plot-partial
-#SBATCH --mem-per-cpu=20G
-#SBATCH --output=2025-05-22-noise-cm-ivp-partial.out
-#SBATCH --array=1-1000
+#SBATCH --time=03:00:00
+#SBATCH --job-name=perfect-mini
+#SBATCH --mem-per-cpu=2G
+#SBATCH --output=2025-05-23-miniajo-partial.out
+#SBATCH --array=1-100
 #SBATCH --cpus-per-task=20
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=elmer.heino@aalto.fi
 
 DATE=$(date +"%Y-%m-%d")
 echo $DATE
-NAME=2025-05-22-noise-cm-ivp
+NAME=2025-05-23-miniajo
 
 CORES=20
 
@@ -20,12 +20,12 @@ module load scicomp-python-env
 NOISE_MIN=-2
 NOISE_MAX=1
 
-ARRAY_LEN=1000   # SEEDS*NOISES=ARRAY_LEN for a square grid
-SEEDS=10         # Seed count is array-max/noise points
+ARRAY_LEN=100   # SEEDS*NOISES=ARRAY_LEN for a square grid
+SEEDS=1         # Seed count is array-max/noise points
 
 TIME=10000
 DT=0.1
 
 # Job step partial dislocation
-srun python3 main.py -p 100 -dt ${DT} -t ${TIME} --partial -f ${WRKDIR}/${NAME}/partial-dislocation -c ${CORES} \
+srun python3 main.py -p 20 -dt ${DT} -t ${TIME} --partial -f ${WRKDIR}/${NAME}/partial-dislocation -c ${CORES} \
     grid --array-task-id ${SLURM_ARRAY_TASK_ID} --rmin ${NOISE_MIN} --rmax ${NOISE_MAX} --array-length ${ARRAY_LEN} --seeds ${SEEDS}
--- run-noise-perfect.sh
+++ run-noise-perfect.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=01:00:00
-#SBATCH --job-name=noise-plot-perfect
+#SBATCH --time=00:30:00
+#SBATCH --job-name=perfect-mini
 #SBATCH --mem-per-cpu=20G
-#SBATCH --output=2025-05-22-noise-cm-ivp-perfect.out
-#SBATCH --array=1-1000
+#SBATCH --output=2025-05-23-miniajo-perfect.out
+#SBATCH --array=1-100
 #SBATCH --cpus-per-task=20
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=elmer.heino@aalto.fi
@@ -11,7 +11,7 @@
 
 DATE=$(date +"%Y-%m-%d")
 echo $DATE
-NAME=2025-05-22-noise-cm-ivp
+NAME=2025-05-23-miniajo
 
 CORES=20
 
@@ -21,12 +21,12 @@ module load scicomp-python-env
 NOISE_MIN=-2
 NOISE_MAX=1
 
-ARRAY_LEN=1000   # SEEDS*NOISES=ARRAY_LEN for a square grid
-SEEDS=10         # Seed count is array-max/noise points
+ARRAY_LEN=100   # SEEDS*NOISES=ARRAY_LEN for a square grid
+SEEDS=1         # Seed count is array-max/noise points
 
 TIME=10000
-DT=0.025
+DT=0.1
 
 # Job step perfect dislocation
-srun python3 main.py -p 100 -dt ${DT} -t ${TIME} --single -f ${WRKDIR}/${NAME}/single-dislocation -c ${CORES} \
+srun python3 main.py -p 20 -dt ${DT} -t ${TIME} --single -f ${WRKDIR}/${NAME}/single-dislocation -c ${CORES} \
     grid --array-task-id ${SLURM_ARRAY_TASK_ID} --rmin ${NOISE_MIN} --rmax ${NOISE_MAX} --array-length ${ARRAY_LEN} --seeds ${SEEDS}
