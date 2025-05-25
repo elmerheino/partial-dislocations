4c019df90f8ff6efad01f0da1622825f72fc22d5
elmerheino <elmer.heino2@gmail.com>
2025-05-25 22:52:57 +0300

    Update run scripts

--- run-noise-perfect.sh
+++ run-noise-perfect.sh
@@ -11,22 +11,24 @@
 
 DATE=$(date +"%Y-%m-%d")
 echo $DATE
-NAME=2025-05-25-miniajo
+NAME=2025-05-25-100-pistetta
 
 CORES=20
 
 # You may put the commands below:
 module load scicomp-python-env
 
-NOISE_MIN=-2
-NOISE_MAX=2
+NOISE_MIN=-3
+NOISE_MAX=3
 
 ARRAY_LEN=100   # SEEDS*NOISES=ARRAY_LEN for a square grid
-SEEDS=1         # Seed count is array-max/noise points
+SEEDS=1         # Seed count is array-max/noise points, so how many seeds per noise level
 
 TIME=10000
 DT=0.1
 
+TAU_POINTS=100   # How many external forces are tried per noise level to find the critical force
+
 # Job step perfect dislocation
-srun python3 main.py -p 40 -dt ${DT} -t ${TIME} --single -f ${WRKDIR}/${NAME}/single-dislocation -c ${CORES} \
+srun python3 main.py -p ${TAU_POINTS} -dt ${DT} -t ${TIME} --single -f ${WRKDIR}/${NAME}/single-dislocation -c ${CORES} \
     grid --array-task-id ${SLURM_ARRAY_TASK_ID} --rmin ${NOISE_MIN} --rmax ${NOISE_MAX} --array-length ${ARRAY_LEN} --seeds ${SEEDS}
