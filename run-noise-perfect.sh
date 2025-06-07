90109b341e4202f862f0fb4cf8ce4c59b92f27bc
elmerheino <elmer.heino2@gmail.com>
2025-06-07 23:01:16 +0300

    Prepare simulation parameters for small noise

--- run-noise-perfect.sh
+++ run-noise-perfect.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=01:30:00
-#SBATCH --job-name=perfect-mini
+#SBATCH --time=10:00:00
+#SBATCH --job-name=perf-region3
 #SBATCH --mem-per-cpu=3G
-#SBATCH --output=2025-05-23-miniajo-perfect.out
-#SBATCH --array=1-100
+#SBATCH --output=2025-06-07-region-3.out
+#SBATCH --array=1-20
 #SBATCH --cpus-per-task=20
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=elmer.heino@aalto.fi
@@ -11,7 +11,7 @@
 
 DATE=$(date +"%Y-%m-%d")
 echo $DATE
-NAME=2025-05-25-100-pistetta
+NAME=2025-06-07-region-3
 
 CORES=20
 
@@ -19,15 +19,15 @@ CORES=20
 module load scicomp-python-env
 
 NOISE_MIN=-3
-NOISE_MAX=3
+NOISE_MAX=-1
 
-ARRAY_LEN=100   # SEEDS*NOISES=ARRAY_LEN for a square grid
+ARRAY_LEN=20   # SEEDS*NOISES=ARRAY_LEN for a square grid
 SEEDS=1         # Seed count is array-max/noise points, so how many seeds per noise level
 
-TIME=10000
+TIME=250000
 DT=0.1
 
-TAU_POINTS=100   # How many external forces are tried per noise level to find the critical force
+TAU_POINTS=40   # How many external forces are tried per noise level to find the critical force
 
 # Job step perfect dislocation
 srun python3 main.py -p ${TAU_POINTS} -dt ${DT} -t ${TIME} --single -f ${WRKDIR}/${NAME}/single-dislocation -c ${CORES} \
