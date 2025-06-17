5d8812818f704882624776ac817eefa3ed1889a4
elmerheino <elmer.heino2@gmail.com>
2025-06-17 21:16:18 +0300

    Update run scripts

--- run-noise-partial.sh
+++ run-noise-partial.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=03:00:00
-#SBATCH --job-name=partial-mini
+#SBATCH --time=24:00:00
+#SBATCH --job-name=partial-reg1
 #SBATCH --mem-per-cpu=2G
-#SBATCH --output=2025-05-23-miniajo-partial.out
-#SBATCH --array=1-100
+#SBATCH --output=2025-06-17-region1-partial.out
+#SBATCH --array=1-10
 #SBATCH --cpus-per-task=20
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=elmer.heino@aalto.fi
@@ -12,7 +12,8 @@
 
 DATE=$(date +"%Y-%m-%d")
 echo $DATE
-NAME=2025-05-25-100-pistetta
+
+NAME=2025-06-17-region-1
 
 CORES=20
 
@@ -20,15 +21,15 @@ CORES=20
 module load scicomp-python-env
 
 NOISE_MIN=-3
-NOISE_MAX=3
+NOISE_MAX=-1
 
-ARRAY_LEN=100   # SEEDS*NOISES=ARRAY_LEN for a square grid
+ARRAY_LEN=10   # SEEDS*NOISES=ARRAY_LEN for a square grid
 SEEDS=1         # Seed count is array-max/noise points
 
-TIME=10000
-DT=0.1
+TIME=250000
+DT=5
 
-TAU_POINTS=100   # How many external forces are tried per noise level to find the critical force
+TAU_POINTS=20   # How many external forces are tried per noise level to find the critical force
 
 # Job step partial dislocation
 srun python3 main.py -p ${TAU_POINTS} -dt ${DT} -t ${TIME} --partial -f ${WRKDIR}/${NAME}/partial-dislocation -c ${CORES} \
