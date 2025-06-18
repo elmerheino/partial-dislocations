4fe53acef99b7f4471048f23c0936e2d00e7220c
elmerheino <elmer.heino2@gmail.com>
2025-06-18 08:35:37 +0300

    update

--- run-noise-perfect.sh
+++ run-noise-perfect.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=2:00:00
 #SBATCH --job-name=perfect-reg1
-#SBATCH --mem-per-cpu=3G
-#SBATCH --output=2025-06-17-region1-perfect.out
+#SBATCH --mem-per-cpu=1G
+#SBATCH --output=2025-06-18-region1-perfect.out
 #SBATCH --array=1-10
 #SBATCH --cpus-per-task=20
 #SBATCH --mail-type=ALL
@@ -12,7 +12,7 @@
 DATE=$(date +"%Y-%m-%d")
 echo $DATE
 
-NAME=2025-06-17-region-1
+NAME=2025-06-18-region-1
 
 CORES=20
 
@@ -25,7 +25,7 @@ NOISE_MAX=-1
 ARRAY_LEN=10   # SEEDS*NOISES=ARRAY_LEN for a square grid
 SEEDS=1         # Seed count is array-max/noise points, so how many seeds per noise level
 
-TIME=250000
+TIME=300000
 DT=5
 
 TAU_POINTS=20   # How many external forces are tried per noise level to find the critical force
