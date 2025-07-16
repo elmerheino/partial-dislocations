ba656e866c777ed52e0c6a0e0b65a347b7e8f742
elmerheino <elmer.heino2@gmail.com>
2025-07-16 09:27:24 +0300

    update run script

--- run-depinnings-from-rel.sh
+++ run-depinnings-from-rel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=00:40:00
-#SBATCH --job-name=depinning-2
-#SBATCH --mem-per-cpu=500M
-#SBATCH --output=depinning-rel.out
+#SBATCH --time=00:45:00
+#SBATCH --job-name=16-7-depinning
+#SBATCH --mem-per-cpu=600M
+#SBATCH --output=16-7-depinning.out
 #SBATCH --cpus-per-task=20
 #SBATCH --array=0-299
 #SBATCH --mail-type=ALL
@@ -10,4 +10,4 @@
 
 module load scicomp-python-env
 
-srun python3 depinningFromRel.py --folder ${WRKDIR}/14-7-relaksaatio/perfect --perfect --points 30 --cores 20 --task-id ${SLURM_ARRAY_TASK_ID} --time 100000 --dt 10 --out-folder ${WRKDIR}/14-7-relaksaatio-try-3/perfect
+srun python3 depinningFromRel.py --folder ${WRKDIR}/14-7-relaksaatio/perfect --perfect --points 30 --cores 20 --task-id ${SLURM_ARRAY_TASK_ID} --time 300000 --dt 10 --out-folder ${WRKDIR}/15-7-tuloksia/single-dislocation
