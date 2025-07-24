8f1673f831826211c72b76c9832bc83270bcde9f
elmerheino <elmer.heino2@gmail.com>
2025-07-24 14:38:03 +0300

    fix error

--- triton-scripts/initial-relaxation-template.sh
+++ triton-scripts/initial-relaxation-template.sh
@@ -12,4 +12,4 @@ module load scicomp-python-env
 TIME={time}
 SYSTEM={system_size}
   
-srun python3 ../initialRelaxations.py new --rmin {rmin} --rmax {rmax} --rpoints {rpoints} --seeds {seeds} --n ${SYSTEM} --length ${SYSTEM} --folder {folder} -c {cores} --d0 {d0} {perfect_partial}
+srun python3 ../initialRelaxations.py new --rmin {rmin} --rmax {rmax} --rpoints {rpoints} --seeds {seeds} --n ${SYSTEM} --length ${SYSTEM} --folder {save_path} -c {cores} --d0 {d0} {perfect_partial}
