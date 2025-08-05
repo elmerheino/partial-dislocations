d589c82864c5b39f25a8d3404e65e871c779e3dd
elmerheino <elmer.heino2@gmail.com>
2025-08-05 10:51:38 +0300

    Save SF data into human readable csv files

--- stackingFault.py
+++ stackingFault.py
@@ -2,6 +2,7 @@ import pickle
 import pandas as pd
 import numpy as np
 from src.core.partialDislocation import PartialDislocationsSimulation
+from pathlib import Path
 
 def extractStackingFaultsFromPickle(pickle_path):
     """
@@ -35,3 +36,44 @@ def extractStackingFaultsFromPickle(pickle_path):
     }
     
     return df
+
+def generateAllSFDatasets(pickle_folder_path):
+    """    Generates stacking fault datasets from pickle files in a given folder.
+        Args:
+            pickle_folder_path (str): Path to the folder containing the pickle files of the simulations.
+
+        This function reads pickle files from the specified folder, extracts stacking fault data,
+        and saves it into a new directory named "stacking-fault-datasets" located in the parent
+        directory of `pickle_folder_path`.
+        Each dataset is saved as a CSV file with the following format:
+
+        +--------------+--------------------+--------------------+----------+--------------------+
+        |   tau_ext    |       time_0       |       time_1       |   ...    |       time_N       |
+        +--------------+--------------------+--------------------+----------+--------------------+
+        | <stress_val> | <sf_width_at_t0>   | <sf_width_at_t1>   |   ...    | <sf_width_at_tN>   |
+        | <stress_val> | <sf_width_at_t0>   | <sf_width_at_t1>   |   ...    | <sf_width_at_tN>   |
+        |      ...     |        ...         |        ...         |   ...    |        ...         |
+        +--------------+--------------------+--------------------+----------+--------------------+
+
+        The filename is generated based on the noise level (deltaR) and the seed value
+        associated with the simulation.  The noise is log transformed for the filename.
+    """ 
+    
+    out_folder = Path(pickle_folder_path).parent / "stacking-fault-datasets"
+    out_folder.mkdir(exist_ok=True)
+    
+    for file in Path(pickle_folder_path).iterdir():
+        if file.suffix == ".pickle":
+            df = extractStackingFaultsFromPickle(file)
+            noise = df.attrs['deltaR']
+            seed = df.attrs['seed']
+                        
+            file_name = f"{np.log(noise):.4f}_noise_stacking_fault_{seed}.csv"
+            file_path = out_folder / file_name
+            
+            df.to_csv(file_path)
+    pass
+
+if __name__ == "__main__":
+    generateAllSFDatasets('results/01-08-2025-depinning/partial/l-64-d0-2.0/depinning-pickle-dumps')
+    pass
